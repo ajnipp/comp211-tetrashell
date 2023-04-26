408d18426307c92b8a87faf3b51eef927ed467b1
A-Nipp <65468669+ajnipp@users.noreply.github.com>
2023-04-26 17:44:43 -0400

    now tokenizing input and can exit

--- tetrashell.c
+++ tetrashell.c
@@ -7,6 +7,8 @@
 #include <unistd.h>
 #include <string.h>
 
+#define MAX_TOKEN 10
+
 int main(int argc, char** argv) {
 
         char buff[FILENAME_MAX];
@@ -20,34 +22,58 @@ int main(int argc, char** argv) {
 		"|_   _|(_))  | |_  ((_)((_)_ / __|| |(_)(_)) | | | |\n"
 		"  | |  / -_) |  _|| '_|/ _` |\\__ \\| ' \\ / -_)| | | |\n"
 		"  |_|  \\___|  \\__||_|  \\__,_||___/|_||_|\\___||_| |_|\n"
-		"the ultimate Tetris quicksave hacking tool!\n\0";
+		"the ultimate Tetris quicksave hacking tool!\n";
 
 		printf("%s", welcomeMessage);
 
 		bool isRunning = true;	
 		bool fileSelected = false;
 		char filePathToHack[FILENAME_MAX];	
+		char* prompt = "tetrashell> ";
+		char* tokens[MAX_TOKEN];
 		while (isRunning) {
 			if (!fileSelected) {
         		printf("Enter the path to the quicksave you'd like to begin hacking: ");
-        		if (fgets(buff, FILENAME_MAX, stdin) == NULL) {
+        		if (fgets(filePathToHack, FILENAME_MAX, stdin) == NULL) {
 					fprintf(stderr, "Error reading input with fgets\n");
 					return EXIT_SUCCESS;
 				}
-				strcpy(filePathToHack, buff);
 				// Now we replace the newline character with the string terminator
 				int len = strlen(filePathToHack);
 				filePathToHack[len-1] = '\0';
 
+				// Check if file path is accessible
 				if (access(filePathToHack, F_OK) == 0) {
-					// File path works, so assign it to our file path variable
 					fileSelected = true;
 				} else {
 					perror("Not a valid file path");
 				}
 			} else {
 				while (fileSelected) {
+					printf("%s", prompt);	
+					if (fgets(buff, FILENAME_MAX, stdin) == NULL) {
+						fprintf(stderr, "Error reading input with fgets\n");
+						return EXIT_SUCCESS;
+					}
+					int tokenCount = 0;
+					char* token = strtok(buff, " ");
+					while( tokenCount < MAX_TOKEN && token != NULL) {
+						tokens[tokenCount] = token;
+						token = strtok(NULL, " ");
+						tokenCount++;
+					}
+					// Strip newline from last token
+					char* lastToken = tokens[tokenCount - 1];
+					lastToken[strlen(lastToken) - 1] = '\0';
 
+					if (tokenCount == 0) {
+						continue;
+					}
+					char* cmd = tokens[0];
+					if (strcmp(cmd, "exit") == 0) {
+						printf("Goodbye!\n");
+						return EXIT_SUCCESS; 
+					}
 				}
 			}
 		}
