f29760dc3ef7904a5228a18d65a7646b5b74a252
A-Nipp <65468669+ajnipp@users.noreply.github.com>
2023-04-26 15:12:40 -0400

    fixed formatting issues with welcome message

--- tetrashell.c
+++ tetrashell.c
@@ -2,28 +2,31 @@
 // Collaborators: Alec Nipp (alecjn)
 
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(int argc, char* argv) {
+int main(int argc, char** argv) {
 
         char *input;
         char buff[FILENAME_MAX];
+		
+		char* welcomeMessage = "Welcome to...\n"
+		"                              (\n"
+		"  *   )          )            )\\ )    )       (   (\n"
+		"` )  /(   (   ( /( (       ) (()/( ( /(    (  )\\  )\\\n"
+		"( )(_)) ))\\  )\\()))(   ( /(  /(_)))\\())  ))\\((_)((_)\n"
+		"(_(_()) /((_)(_))/(()\\  )(_))(_)) ((_)\\  /((_)_   _\n"
+		"|_   _|(_))  | |_  ((_)((_)_ / __|| |(_)(_)) | | | |\n"
+		"  | |  / -_) |  _|| '_|/ _` |\\__ \\| ' \\ / -_)| | | |\n"
+		"  |_|  \\___|  \\__||_|  \\__,_||___/|_||_|\\___||_| |_|\n"
+		"the ultimate Tetris quicksave hacking tool!\n\0";
 
-        printf("Welcome to...\n");
-        printf("                              (                       
-  *   )          )            )\ )    )       (   (   
-` )  /(   (   ( /( (       ) (()/( ( /(    (  )\  )\  
- ( )(_)) ))\  )\()))(   ( /(  /(_)))\())  ))\((_)((_) 
-(_(_()) /((_)(_))/(()\  )(_))(_)) ((_)\  /((_)_   _   
-|_   _|(_))  | |_  ((_)((_)_ / __|| |(_)(_)) | | | |  
-  | |  / -_) |  _|| '_|/ _` |\__ \| ' \ / -_)| | | |  
-  |_|  \___|  \__||_|  \__,_||___/|_||_|\___||_| |_|\n");
-        printf("the ultimate Tetris quicksave hacking tool!\n");
+		printf("%s", welcomeMessage);
         printf("Enter the path to the quicksave you'd like to begin hacking: ");
 
         input = fgets(buff, FILENAME_MAX, stdin);
         if (input == NULL) {
                 fprintf(stderr, "error with fgets");
-                return EXIT_SUCCESS:
+                return EXIT_SUCCESS;
         }
 
         printf("%s\n", input);
