db2398ad497e72bb27b6604befce0cee02c584da
A-Nipp <65468669+ajnipp@users.noreply.github.com>
2023-04-26 17:08:57 -0400

    now checking if file exists

--- tetrashell.c
+++ tetrashell.c
@@ -3,10 +3,12 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
+#include <unistd.h>
+#include <string.h>
 
 int main(int argc, char** argv) {
 
-        char *input;
         char buff[FILENAME_MAX];
 		
 		char* welcomeMessage = "Welcome to...\n"
@@ -21,16 +23,33 @@ int main(int argc, char** argv) {
 		"the ultimate Tetris quicksave hacking tool!\n\0";
 
 		printf("%s", welcomeMessage);
-        printf("Enter the path to the quicksave you'd like to begin hacking: ");
 
-        input = fgets(buff, FILENAME_MAX, stdin);
-        if (input == NULL) {
-                fprintf(stderr, "error with fgets");
-                return EXIT_SUCCESS;
-        }
+		bool isRunning = true;	
+		bool fileSelected = false;
+		char filePathToHack[FILENAME_MAX];	
+		while (isRunning) {
+			if (!fileSelected) {
+        		printf("Enter the path to the quicksave you'd like to begin hacking: ");
+        		if (fgets(buff, FILENAME_MAX, stdin) == NULL) {
+					fprintf(stderr, "Error reading input with fgets\n");
+					return EXIT_SUCCESS;
+				}
+				strcpy(filePathToHack, buff);
+				// Now we replace the newline character with the string terminator
+				int len = strlen(filePathToHack);
+				filePathToHack[len-1] = '\0';
 
-        printf("%s\n", input);
+				if (access(filePathToHack, F_OK) == 0) {
+					// File path works, so assign it to our file path variable
+					fileSelected = true;
+				} else {
+					perror("Not a valid file path");
+				}
+			} else {
+				while (fileSelected) {
 
-
+				}
+			}
+		}
 }
 
